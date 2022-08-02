a8c9d4eb422f6438471a46f0a114627ec73f9ba3
Agni_4 <68882114+vishuparam@users.noreply.github.com>
2022-08-02 08:40:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var FirstName = "Vishu"
+var LastName = "Paramkusham"
+var FullName = FirstName.concat(" ").concat(LastName)
+console.log(FullName);
+
+var MathVar1 = 5;
+var MathVar2 = 10;
+
+var MathAdd = MathVar1 + MathVar2
+console.log(MathAdd)
+
+var MathMulti = MathVar1 * MathVar2
+console.log(MathMulti)
+
+var MathSub = MathVar2 - MathVar1
+console.log(MathSub)
+
+var MathDiv = MathVar2 / MathVar1
+console.log(MathDiv)
